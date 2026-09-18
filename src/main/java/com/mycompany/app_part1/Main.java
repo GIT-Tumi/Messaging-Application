89d3115cadb3cefbfa8885d5bf61e512e0810286
GIT-Tumi <st10530449@rcconnect.edu.za>
2026-09-18 14:23:08 +0200

    file naming

---
+++ src/main/java/com/mycompany/app_part1/Main.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.app_part1;
+
+/**
+ *
+ * @author tumi
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
