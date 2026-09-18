4dcb35e918d5b8bad0ff864e657213919a785c06
GIT-Tumi <st10530449@rcconnect.edu.za>
2026-09-18 14:16:27 +0200

    Add files via upload
    changing to java with maven

---
+++ App_part1/src/main/java/com/mycompany/app_part1/App_part1.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.app_part1;
+
+/**
+ *
+ * @author tumi
+ */
+public class App_part1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
