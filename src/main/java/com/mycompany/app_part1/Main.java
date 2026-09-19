bb34387157973cfd7094560fdcf91aae63f619e4
GIT-Tumi <st10530449@rcconnect.edu.za>
2026-09-19 02:00:16 +0200

    Declerations amd login file created

--- src/main/java/com/mycompany/app_part1/Main.java
+++ src/main/java/com/mycompany/app_part1/Main.java
@@ -11,6 +11,20 @@ package com.mycompany.app_part1;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        // TODO code application logic here
+     // Create a Login object
+     Login user = new Login("Tumisho", "Mokoena", "tum_1", "Pass@1234!", "+27838968976");
+
+     // Try registering the user
+     System.out.println(user.registerUser());
+
+     // Attempt login with correct credentials
+     boolean loggedIn = user.loginUser("tum_1", "Pass@1234!");
+     System.out.println(user.returnLoginStatus());
+
+     // Attempt login with wrong password
+     user.loginUser("tum_1", "wrongPassword");
+     System.out.println(user.returnLoginStatus());    
+    
     }
 }
